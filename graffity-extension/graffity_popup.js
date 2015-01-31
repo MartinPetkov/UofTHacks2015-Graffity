be7d290a794c8c9386056b058231554be70e43ab
Martin Petkov <martin.petkov@mail.utoronto.ca>
2015-01-31 09:57:50 -0500

    Got an AJAX call working! Still gotta figure out how to circumvent the security issue, but if I can get requests working back and forth between the server and the extension by 12, I'll be happy.

--- graffity-extension/graffity_popup.js
+++ graffity-extension/graffity_popup.js
@@ -1,3 +1,45 @@
+var tab_url = "";
+
+function startDrawing() {
+    alert("Start Drawing button was clicked");
+}
+
+function stahpDrawing() {
+    alert("Stop Drawing button was clicked");
+}
+
+function saveGraffiti() {
+    var graffitiName = document.getElementById("graffiti-name").value;
+    alert("Save Graffiti with name: " + graffitiName);
+}
+
+function loadGraffiti() {
+    var req = new XMLHttpRequest();
+    var url = "http://127.0.0.1:8000/graffity/get_drawings/";
+
+    req.onreadystatechange = function() {
+        if(req.readyState == 4 && req.status == 200) {
+            var graffitiArr = JSON.parse(req.responseText);
+            alert(graffitiArr);
+        }
+    }
+
+    req.open("GET", url, true);
+    req.setRequestHeader('req_graffiti_url', tab_url);
+    req.send();
+
+}
+
+
 window.onload = function() {
-    console.log("Gooby pls");
+    document.getElementById("start-drawing-button").onclick = startDrawing;
+    document.getElementById("stop-drawing-button").onclick = stahpDrawing;
+    document.getElementById("save-graffiti-button").onclick = saveGraffiti;
+    document.getElementById("load-graffiti-button").onclick = loadGraffiti;
+
+    chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+        function(tabs){
+            tab_url = tabs[0].url;
+        }
+    );
 };
